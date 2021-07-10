262d35b00711edc0da6be2fe342a038566f80c91
김태섭 <kyoer234@gmail.com>
2021-07-10 14:49:42 +0900

    이전화면으로 복귀 : finish()

--- app/src/main/java/com/nepplus/intent_20210710/OtherActivity.kt
+++ app/src/main/java/com/nepplus/intent_20210710/OtherActivity.kt
@@ -1,11 +1,18 @@
 package com.nepplus.intent_20210710
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+        moveToMainBtn.setOnClickListener {
+//            val otherIntent = Intent(this,MainActivity::class.java)
+//            startActivity(otherIntent);
+            finish();
+        }
     }
 }
