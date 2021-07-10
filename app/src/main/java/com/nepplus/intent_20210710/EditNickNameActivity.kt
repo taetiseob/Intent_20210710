6899605387ecb0957f30ed40f8814d99f06688aa
김태섭 <kyoer234@gmail.com>
2021-07-10 16:01:41 +0900

    완료버튼 누르면 복귀  - 데이터 첨부만 안한 상태.

--- app/src/main/java/com/nepplus/intent_20210710/EditNickNameActivity.kt
+++ app/src/main/java/com/nepplus/intent_20210710/EditNickNameActivity.kt
@@ -2,10 +2,20 @@ package com.nepplus.intent_20210710
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
 
 class EditNickNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nick_name)
+        edtBtn.setOnClickListener {
+            //입력한 새 닉네임이 뭔지
+            val newNickname = nicknameEdt.text.toString()
+            //그걸 돌아가는 길에 첨부하고(결과로 설정한다)
+
+            //이전화면으로 복귀
+            finish()
+        }
     }
+
 }
