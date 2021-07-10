309e2612d413715dbdbb7f69397391be97c1a162
김태섭 <kyoer234@gmail.com>
2021-07-10 15:49:27 +0900

    닉네임 변경화면 추가

---
+++ app/src/main/java/com/nepplus/intent_20210710/EditNickNameActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.intent_20210710
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNickNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nick_name)
+    }
+}
